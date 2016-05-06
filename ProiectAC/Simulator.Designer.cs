f7672869b4e00836f0e7ec6cb2de58248dab4c71
samip <samuel.pallo@indsoft.ro>
2016-05-06 15:43:36 +0300

    project added

---
+++ ProiectAC/Simulator.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ProiectAC
+{
+    partial class Simulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PrincipalShape = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.openAsmFile = new System.Windows.Forms.Button();
+            this.assembleFile = new System.Windows.Forms.Button();
+            this.instrBox = new System.Windows.Forms.ListBox();
+            this.assembleBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // PrincipalShape
+            // 
+            this.PrincipalShape.FillColor = System.Drawing.Color.White;
+            this.PrincipalShape.FillGradientColor = System.Drawing.Color.White;
+            this.PrincipalShape.Location = new System.Drawing.Point(257, 99);
+            this.PrincipalShape.Name = "";
+            this.PrincipalShape.SelectionColor = System.Drawing.Color.White;
+            this.PrincipalShape.Size = new System.Drawing.Size(835, 479);
+            // 
+            // openAsmFile
+            // 
+            this.openAsmFile.Location = new System.Drawing.Point(40, 12);
+            this.openAsmFile.Name = "openAsmFile";
+            this.openAsmFile.Size = new System.Drawing.Size(75, 23);
+            this.openAsmFile.TabIndex = 0;
+            this.openAsmFile.Text = "Open asn file";
+            this.openAsmFile.UseVisualStyleBackColor = true;
+            this.openAsmFile.Click += new System.EventHandler(this.openAsmFile_Click);
+            // 
+            // assembleFile
+            // 
+            this.assembleFile.Location = new System.Drawing.Point(178, 12);
+            this.assembleFile.Name = "assembleFile";
+            this.assembleFile.Size = new System.Drawing.Size(75, 23);
+            this.assembleFile.TabIndex = 1;
+            this.assembleFile.Text = "Assemble";
+            this.assembleFile.UseVisualStyleBackColor = true;
+            this.assembleFile.Click += new System.EventHandler(this.assembleFile_Click);
+            // 
+            // instrBox
+            // 
+            this.instrBox.FormattingEnabled = true;
+            this.instrBox.Location = new System.Drawing.Point(12, 51);
+            this.instrBox.Name = "instrBox";
+            this.instrBox.Size = new System.Drawing.Size(211, 511);
+            this.instrBox.TabIndex = 2;
+            // 
+            // assembleBox
+            // 
+            this.assembleBox.FormattingEnabled = true;
+            this.assembleBox.Location = new System.Drawing.Point(12, 568);
+            this.assembleBox.Name = "assembleBox";
+            this.assembleBox.Size = new System.Drawing.Size(211, 17);
+            this.assembleBox.TabIndex = 3;
+            // 
+            // Simulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1063, 588);
+            this.Controls.Add(this.assembleBox);
+            this.Controls.Add(this.instrBox);
+            this.Controls.Add(this.assembleFile);
+            this.Controls.Add(this.openAsmFile);
+            this.Name = "Simulator";
+            this.RightToLeftLayout = true;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public Microsoft.VisualBasic.PowerPacks.RectangleShape PrincipalShape;
+        private System.Windows.Forms.Button openAsmFile;
+        private System.Windows.Forms.Button assembleFile;
+        public System.Windows.Forms.ListBox instrBox;
+        public System.Windows.Forms.ListBox assembleBox;
+    }
+}
+
---
+++ ProiectAC/Variable.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectAC
+{
+    class Variable
+    {
+        private struct label
+        {
+            public string name;
+            public int address;
+        };
+
+        private struct procedure
+        {
+            public string name;
+            public int address;
+        };
+
+        private label[] labels = new label[50];
+        private procedure[] procedures = new procedure[50];
+
+        private byte[] RAM = new byte[65536];
+        private UInt16 IR = 0;
+        private UInt16 FLAG = 0;
+        private UInt16 IVR = 0;
+        private UInt16 T = 0;
+        private UInt16 SP = 65534;
+        private UInt16 MDR = 0;
+        private UInt16 ADR = 0;
+        private UInt16 PC = 0;
+        private UInt16 PCMax = 0; // PC Maxim, in functie de cate instructiuni sunt in fisier
+        private UInt16[] R = new UInt16[16];
+        private UInt16 reg = 0;
+        private UInt16 g = 0;
+        private UInt16 clasa;
+        private UInt16 IndexValoare;
+
+        private UInt64[] MPM = new UInt64[200];
+        private UInt64 MIR = 0;
+        private UInt16 MAR = 0;
+        private UInt16 SBUS = 0;
+        private UInt16 DBUS = 0;
+        private UInt16 RBUS = 0;
+        private UInt16 Carry = 0;
+
+        private int Step = 0;
+
+        private UInt16 bit = 0;
+
+        private string registru;
+        private string valoareImediata;
+        private string valImediataBinar;
+        private string IM = "00";
+        private string AD = "01";
+        private string AI = "10";
+        private string AX = "11";
+
+        private int contor;
+        private int adresaSalt;
+        private int modAdresare;
+        private int offset;
+        private string valIndex;
+        private string valIndexSursa;
+        private string valIndexDest;
+        private string offsetString;
+
+        Dictionary<string, string> base1 = new Dictionary<string, string>();
+        Dictionary<string, string> base2 = new Dictionary<string, string>();
+        Dictionary<string, string> base3 = new Dictionary<string, string>();
+        Dictionary<string, string> base4 = new Dictionary<string, string>();
+        Dictionary<string, string> registers = new Dictionary<string, string>() {
+            {"R0","0000"},{"R1","0001"},{"R2","0010"},{"R3","0011"},{"R4","0100"},{"R5","0101"},{"R6","0110"},{"R7","0111"},
+            {"R8","1000"},{"R9","1001"},{"R10","1010"},{"R11","1011"},{"R12","1100"},{"R13","1101"},{"R14","1110"},{"R15","1111"}
+        };
+        Dictionary<string, string> registreIndex = new Dictionary<string, string>();
+        Dictionary<UInt16, byte> mem = new Dictionary<UInt16, byte>();
+
+        List<string> prelines = new List<string>();
+        List<string> instructions = new List<string>();
+
+        List<string> microcod = new List<string>();
+
+        string opcode = null;
+        string codif;
+        int numberLabels = 0;
+        int numberProcedures = 0;
+
+        private bool isbase1 = false;
+        private bool isbase2 = false;
+        private bool isbase3 = false;
+        private bool isbase4 = false;
+        private bool isValImm = false;
+        private bool isIndexSrc = false;
+        private bool isIndexDest = false;
+        private bool isError = false;
+        private bool isJMP = false;
+        private bool isCALL = false;
+        private bool isPUSHRi = false;
+        private bool isPOPRi = false;
+        bool asmFileOpened = false;
+        bool microCodeOpened = false;
+        bool compiledOk;
+        private bool isStep;
+        private bool isINT;
+
+    }
+}
